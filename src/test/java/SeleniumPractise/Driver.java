cb27fa044e38964c7e763ec58446131a190e495b
abgitt <abhilash.ballavolu@gmail.com>
2018-09-08 16:09:21 +0530

    Project is created and my first commit

---
+++ src/test/java/SeleniumPractise/Driver.java
@@ -0,0 +1,17 @@
+package SeleniumPractise;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Driver {
+	
+	@Test
+	public void startChrome()
+	{
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver dr=new ChromeDriver();
+		dr.get("http://www.youtube.com");
+	}
+
+}
