e9683eb623f49e6056b2e97087808830e0ff15c7
abgitt <abhilash.ballavolu@gmail.com>
2018-09-08 16:35:54 +0530

    Second

--- src/test/java/SeleniumPractise/Driver.java
+++ src/test/java/SeleniumPractise/Driver.java
@@ -2,6 +2,7 @@ package SeleniumPractise;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class Driver {
@@ -13,5 +14,9 @@ public class Driver {
 		WebDriver dr=new ChromeDriver();
 		dr.get("http://www.youtube.com");
 	}
-
+	@AfterTest
+	public void closeChrome()
+	{
+		System.out.println("Closing ");
+	}
 }
---
+++ src/test/java/SeleniumPractise/NewClass.java
@@ -0,0 +1,12 @@
+package SeleniumPractise;
+
+import org.testng.annotations.Test;
+
+public class NewClass {
+
+	@Test
+	public void newC()
+	{
+		System.out.println("HI");
+	}
+}
