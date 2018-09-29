e2b18287d9068e0b3fcc8fb82906d2d76c0d286b
abgitt <abhilash.ballavolu@gmail.com>
2018-09-29 22:57:50 +0530

    Check

--- src/test/java/SeleniumPractise/JavascriptExecutorConcept.java
+++ src/test/java/SeleniumPractise/JavascriptExecutorConcept.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-
+//Java Script executor
 public class JavascriptExecutorConcept {
 		public static void main(String args[]) throws InterruptedException, IOException
 		{
